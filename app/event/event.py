a19312d75fda08491fd44bc4b6faed4b186a46dd
Seonghyeon Kim <self@seonghyeon.dev>
2021-05-16 18:15:02 +0900

    Update event bases

--- app/event/event.py
+++ app/event/event.py
@@ -7,15 +7,14 @@ from uuid import UUID
 class Event(ABC):
     occurred_at: datetime
 
+    def __post_init__(self):
+        self.occurred_at = get_utc_datetime()
+
 
 class DomainEvent(Event):
     source_id: UUID
     version: int
 
-    def __post_init__(self):
-        self.occurred_at = get_utc_datetime()
-
 
 class IntegrationEvent(Event):
-    source: str
-    destination: str
+    pass
